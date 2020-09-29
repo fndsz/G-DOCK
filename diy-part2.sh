5b0cbe8689484a6eddfd4434da75e126bcd4c295
fndsz <45010248+fndsz@users.noreply.github.com>
2020-09-29 09:54:31 +0800

    Add files via upload

---
+++ diy-part2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+device_name='ZTE-E8820S'
+wifi_name='ZET'
+lan_ip='192.168.1.1'        # Lan Ip地址
+utc_name='Asia\/Shanghai'   # 时区
+ 
+#修改机器名称
+echo "设置主机名"
+sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+
+
+# Modify default IP
+echo "设置lan ip"
+sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+
+
+#修改默认时区
+echo "修改时区"
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+echo '修改banner'
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
+
+echo "修改wifi名称"
+
+sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+echo "替换文件"
+
+rm -rf ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network && cd .. && cp -f ./02_network openwrt/target/linux/ramips/mt7621/base-files/etc/board.d/ && cd openwrt
+
