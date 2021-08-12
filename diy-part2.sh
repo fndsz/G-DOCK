97129f1334997f87aea3995d69b0806c315484a2
fndsz <45010248+fndsz@users.noreply.github.com>
2021-08-12 11:52:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,8 +16,6 @@ sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generat
 # Modify hostname
 sed -i 's/OpenWrt_/G-DOCK/g' package/base-files/files/bin/config_generate
 
-
-
 # 取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
